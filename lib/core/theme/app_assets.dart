056d4a9a8b79286b0cebe41dce54e9eceb742133
Alex-Yu21 <yualex2112@gmail.com>
2026-01-25 19:37:18 +0100

    AppAssets class with assets paths

---
+++ lib/core/theme/app_assets.dart
@@ -0,0 +1,17 @@
+class AppAssets {
+  AppAssets._();
+
+  static const String _png = 'assets/png';
+  static const String _svg = 'assets/svg';
+
+  // PNG
+  static const String banner = '$_png/banner.png';
+  static const String download = '$_png/download.png';
+  static const String flashlightCamera = '$_png/flashlight_camera.png';
+  static const String ios26 = '$_png/ios26.png';
+  static const String live = '$_png/live.png';
+  static const String wallpaper = '$_png/wallpaper.png';
+
+  // SVG
+  static const String aiIcon = '$_svg/ai_icon.svg';
+}
